609c2f72173ebe088071e021e0c6eccfb3021b6f
杨浩 <501625574@qq.com>
2020-04-28 20:25:30 +0800

    consumer 80

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/config/ApplicationContextConfig.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/config/ApplicationContextConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+/**
+ * 由于springboot是个容器类，而且我们需要RestTemplate类，所以我们要写一个配置类将RestTemplate注入到springboot容器中
+ */
 @Configuration
 public class ApplicationContextConfig {
 
