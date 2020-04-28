a5c040e557daa80e73bd5cae1ba4713f95eadee8
杨浩 <501625574@qq.com>
2020-04-28 20:12:18 +0800

    consumer 80

---
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/entities/Payment.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud.entities;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+//@Deprecated
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Payment implements Serializable {
+
+    private long id;
+    private String serial;
+}
